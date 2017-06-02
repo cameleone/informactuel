ab2a97938d61701ce383befb9d1efdaed8fcbbb4
cameleone <strass.comagency@gmail.com>
2017-06-02 08:21:25 +0300

    Update settings.py

--- drf_sample/drf_sample/settings.py
+++ drf_sample/drf_sample/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_sample.retail',
     'rest_framework',
     'client',
     'inform_actuel',
